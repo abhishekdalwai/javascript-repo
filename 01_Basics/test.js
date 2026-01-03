b10ef287127ba9429d202ed5b3a86b70287811be
abhishekdalwai <abhishek_durdundeshvar.dalwai@contractors.roche.com>
2026-01-03 11:52:42 +0000

    we

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,9 @@
 console.log("Hello World")
+
+const accountId = "12345"  // contsant value does not chabng ethrought the code
+let accountemail = "abhishekfdalwai98@gmail.com" 
+var accountPassword = "12345" // this creates a problem when when refering in if or else so let came into picture
+accountcity = "pune"
+
+
+console.log(accountcity)
